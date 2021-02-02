633ac2002605ce53e329039980d07fc2ac299c15
ken025 <kennethmanuel250@gmail.com>
2021-02-01 22:20:34 -0500

    tested convertions and 'try catch'

--- Program.cs
+++ Program.cs
@@ -6,7 +6,34 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-         System.Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+         int i = 1;
+         float f = i;
+
+        Console.WriteLine(i);
+        Console.WriteLine(f);
+
+        int a = 2;
+        byte b = (byte) a;
+
+        Console.WriteLine(b);
+
+        var number = "123";
+        int n = Convert.ToInt32(number);
+
+        System.Console.WriteLine(n);
+
+        try
+        {
+            var num = "1234";
+            byte s  = Convert.ToByte(num);
+            System.Console.WriteLine(s);
+        
+        }
+        catch (System.Exception)
+        {
+            System.Console.WriteLine("The number cannot be converted into a byte");
+        }
+        
         }
     }
 }
