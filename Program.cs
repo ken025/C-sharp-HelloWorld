4c50f1faff1a00882e4b7a8202f2a7da1058cd09
ken025 <kennethmanuel250@gmail.com>
2021-02-01 21:27:43 -0500

    testing types mins and maxs

--- Program.cs
+++ Program.cs
@@ -6,19 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            byte number = 20;
-            int count = 10;
-            float totalPrice = 20.95f;
-            char character = 'A';
-            string firstName = "Ken";
-            bool isWorking = false;
-
-            Console.WriteLine(number);
-           Console.WriteLine(count);
-           Console.WriteLine(totalPrice);
-           Console.WriteLine(character);
-           Console.WriteLine(firstName);
-           Console.WriteLine(isWorking);
+         System.Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
         }
     }
 }
