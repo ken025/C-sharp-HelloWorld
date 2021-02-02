13a9097cd307beac29558cefa3ea367e61128a0d
ken025 <kennethmanuel250@gmail.com>
2021-02-01 23:10:36 -0500

    tested increment, decrement, and conditionals

--- Program.cs
+++ Program.cs
@@ -34,6 +34,24 @@ namespace HelloWorld
             System.Console.WriteLine("The number cannot be converted into a byte");
         }
         
+        
+        int k = 1;
+        int m = k++;
+
+        System.Console.WriteLine(k);
+        System.Console.WriteLine(m);
+
+        int k2 = 1;
+        int m2 = ++k2;
+
+        System.Console.WriteLine(k2);
+        System.Console.WriteLine(m2);
+
+        var numb = 1;
+        var numbe = 2;
+        var nmbr = 3; 
+
+        System.Console.WriteLine(!(nmbr > numbe || nmbr == numb));
         }
     }
 }
