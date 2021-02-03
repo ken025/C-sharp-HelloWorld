269f94f86e0b5745140620b547347487564c8e54
ken025 <kennethmanuel250@gmail.com>
2021-02-02 23:26:43 -0500

    working with iteration statements

--- Program.cs
+++ Program.cs
@@ -8,50 +8,21 @@ namespace HelloWorld
     {
         static void  Main(string[] args)
         {
-            int hour = 10;
+          for( var i = 1; i <= 10; i++){
+              if (i%2 == 0){
+                  System.Console.WriteLine(i);
+              }
+          }
 
-            if (hour > 8 && hour < 12)
-            {
-            System.Console.WriteLine("It's morning");
-            }
-            else if (hour >= 12 && hour < 18)
-            {
-            System.Console.WriteLine("It's evening");
-            }
-            else 
-            {
-                System.Console.WriteLine("It's afternoon");
-            }
+          var name = "Kenneth";
 
-            bool isCostumer = true;
+          for (var i = 0; i < name.Length; i++){
+              System.Console.WriteLine(name[i]);
+          }
 
-            // float price;
-            // if (isCostumer)
-            //     price = 19.95f;
-            // else 
-            //     price = 29.95f;
-
-            float price = (isCostumer) ? 19.95f : 29.95f;
-
-            System.Console.WriteLine(price);
-
-
-            var season = Season.Autumn; 
-
-            switch (season)
-            {
-                case Season.Autumn:
-                    System.Console.WriteLine("It's Autumn!");
-                break;
-
-                case Season.Summer:
-                    System.Console.WriteLine("It's Summer");
-                break;
-
-                default:
-                    System.Console.WriteLine("I'm not sure");
-                break;
-            }
+          foreach(var character in name){
+              System.Console.WriteLine(character);
+          }
 
          }
     }
